f8b482a39af1089677a38dfb2d4d3c8e3c14a57d
Z1rael88 <oleksiibahmet@gmail.com>
2024-07-04 14:48:11 +0200

    gitignore update

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Dto;
 using WebApplication1.Dtos;
+using WebApplication1.Dtos.SearchParams;
 using WebApplication1.Dtos.TaskDtos;
 using WebApplication1.Interfaces.TaskRepositories;
 
 namespace WebApplication1.Controllers;
+
 [Route("api/[controller]")]
 [ApiController]
 public class TasksController(ITaskService taskService) : ControllerBase
@@ -12,9 +13,9 @@ public class TasksController(ITaskService taskService) : ControllerBase
     private readonly ITaskService _taskService = taskService ?? throw new ArgumentNullException(nameof(taskService));
 
     [HttpGet]
-    public async Task<IEnumerable<TaskResponse>> GetAllAsync([FromQuery] SearchDto searchDto)
+    public async Task<IEnumerable<TaskResponse>> GetAllAsync([FromQuery] TaskSearchParams taskSearchParams)
     {
-        return await _taskService.GetAllAsync(searchDto);
+        return await _taskService.GetAllAsync(taskSearchParams);
     }
 
     [HttpGet("{id}")]
@@ -24,7 +25,7 @@ public class TasksController(ITaskService taskService) : ControllerBase
     }
 
     [HttpPost]
-    public async Task<TaskResponse> CreateTaskAsync([FromBody]TaskRequest taskRequest,Guid projectId)
+    public async Task<TaskResponse> CreateTaskAsync([FromBody] TaskRequest taskRequest, Guid projectId)
     {
         return await _taskService.CreateAsync(taskRequest, projectId);
     }
@@ -38,6 +39,6 @@ public class TasksController(ITaskService taskService) : ControllerBase
     [HttpDelete("{id}")]
     public async Task DeleteTaskAsync(Guid id)
     {
-         await _taskService.DeleteAsync(id);
+        await _taskService.DeleteAsync(id);
     }
 }

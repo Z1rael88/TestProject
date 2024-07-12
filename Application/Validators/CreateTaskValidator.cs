a3332f995e9d8bcb6c84298ca88488c215b92a74
Z1rael88 <oleksiibahmet@gmail.com>
2024-07-12 11:08:58 +0200

    fixed Validator

--- Application/Validators/CreateTaskValidator.cs
+++ Application/Validators/CreateTaskValidator.cs
@@ -1,13 +1,18 @@
 using Application.Dtos.TaskDtos;
+using Domain.ValidationOptions;
 using FluentValidation;
+using Microsoft.Extensions.Options;
 
 namespace Application.Validators;
 
 public class CreateTaskValidator : AbstractValidator<CreateTaskRequest>
 {
-    public CreateTaskValidator()
+    public CreateTaskValidator( IOptions<TaskValidationOptions> validationOptions)
     {
+        Include(new TaskValidator(validationOptions));
         RuleFor(p => p.ProjectId)
-            .NotEmpty().WithMessage("Task must have Project Id");
+            .NotEmpty().WithMessage("Task must have Project Id")
+            .Must(id => Guid.TryParse(id.ToString(), out _))
+            .WithMessage("Project Id must be a valid GUID");
     }
 }
